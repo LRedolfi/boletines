de24b14ea999f2773a77762be9a1656b04b35d1a
Luciano <luchoredolfi@gmail.com>
2021-10-19 19:30:26 -0300

    Eventos

--- javascript/principal.js
+++ javascript/principal.js
@@ -1,6 +1,24 @@
 //DOM
-let links=document.querySelectorAll("a");
+/*let links=document.querySelectorAll("a");
 
 links.forEach(function(link){
     console.log(link);
+});*/
+
+/*let celdas=document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+    td.addEventListener('click',function(){
+        console.log(this);
+    })
+});*/
+
+//Obtener los elementos de la clase .cerrar
+let links=document.querySelectorAll(".cerrar");
+//recorrerlos
+links.forEach(function(link){
+    //Agregar un evento click a cada uno de ellos
+    link.addEventListener("click",function(){
+        console.log(":)")
+    });
 });
