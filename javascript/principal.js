a1857490cef0f8fbf207cd0cc3734f60119817c6
Luciano <luchoredolfi@gmail.com>
2021-10-21 19:04:21 -0300

    Quitar y agregar clases a  elemento con Javascript

--- javascript/principal.js
+++ javascript/principal.js
@@ -1,24 +1,17 @@
-//DOM
-/*let links=document.querySelectorAll("a");
-
-links.forEach(function(link){
-    console.log(link);
-});*/
-
-/*let celdas=document.querySelectorAll("td");
-
-celdas.forEach(function(td){
-    td.addEventListener('click',function(){
-        console.log(this);
-    })
-});*/
-
 //Obtener los elementos de la clase .cerrar
 let links=document.querySelectorAll(".cerrar");
 //recorrerlos
 links.forEach(function(link){
     //Agregar un evento click a cada uno de ellos
-    link.addEventListener("click",function(){
+    link.addEventListener("click",function(ev){
         console.log(":)")
+        ev.preventDefault();
+        let contenido=document.querySelector(".content");
+        //Quitarle las clases de animacion que tiene
+        contenido.classList.remove("animate__animated");
+        contenido.classList.remove("animate__fadeInDown");
+        //Agregar clase de animacion para salida animate__fadeInUp
+        contenido.classList.add("animate__animated");
+        contenido.classList.add("animate__fadeOutUp");
     });
 });
